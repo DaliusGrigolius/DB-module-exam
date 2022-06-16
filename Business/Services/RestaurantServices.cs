cd8661c60405e837b46e9f4b737c963901c79b80
Dalius Grigolius <dalius.grigolius@codeacademylt.onmicrosoft.com>
2022-06-16 17:47:17 +0200

    add services. wip

---
+++ Business/Services/RestaurantServices.cs
@@ -0,0 +1,19 @@
+﻿using Repository.DbContexts;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class RestaurantServices
+    {
+        public void CreateRestaurant()
+        {
+            RestaurantDbContext rdbc = new RestaurantDbContext();
+            rdbc.Add(new Restaurant());
+        }
+    }
+}
